137680550a23d6551126e3cf479b930ccd0da638
Alexina Chong <alexina.chong@gmail.com>
2018-06-01 23:06:28 -0700

    Implement Pokemon index

---
+++ pokedex/frontend/components/pokemon/pokemon_index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class PokemonIndex extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      this.props.pokemon.map(poke => {
+
+      })
+
+    );
+  }
+}
+
+export default PokemonIndex;
