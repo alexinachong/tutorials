81248999ada40c3ced3898443e50bf83429bba0b
Alexina Chong <alexina.chong@gmail.com>
2018-08-14 23:28:45 -0700

    Add Cat model validations and migrate

--- 99cats/ninetyninecats/app/models/cat.rb
+++ 99cats/ninetyninecats/app/models/cat.rb
@@ -1,2 +1,15 @@
 class Cat < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
+  validates :birth_date, presence: true
+  validates :color, presence: true, inclusion: CAT_COLORS
+  validates :name, presence: true
+  validates :sex, presence: true, inclusion: %w(M F)
+  validates :description, presence: true
+
+  CAT_COLORS = %w(black brown orange white).freeze
+
+  def age
+    time_ago_in_words(birth_date)
+  end
 end
